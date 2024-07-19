e57ea2ad3041a7e0274add05f40a903928afdd66
parkhar <pkatiyar574@gmail.com>
2024-07-19 16:53:12 +0530

    hello

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
+// vite.config.js
+export default {
   build: {
-    outDir: 'dist'
-  }
-});
+    outDir: 'dist',
+  },
+  // other Vite config options...
+};
